9c243c8a0ab8934d6ce1b25fe5a942bbb1fd31c1
Haruino <harukiinoue5270@gmail.com>
2024-02-24 02:58:55 +0000

    [Start]応用課題5

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,6 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
-  # フォローする側
+  # フォローする側、架空のfollowerテーブルに属すると仮定
   belongs_to :followed, class_name: "User"
-  # フォローされる側
+  # フォローされる側、架空のfollowedテーブルに属すると仮定
 end
