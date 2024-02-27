bec8ab68e0226942bb9a76c92edf2482d4152cf1
Haruino <harukiinoue5270@gmail.com>
2024-02-27 13:01:36 +0000

    [Finish]応用課題6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,8 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
      # ローカル変数 = モデルを指定し.固有の[idカラム]を取得し(find)どの本にいいねをするか決める
-    favorite = current_user.favorites.new(book_id: book.id)
+    favorite = current_user.favorites.new(book_id: @book.id)
     # current_userで現在のユーザーを指定し、favorites.newで現在ログイン中のユーザーがいいねできるようにbook_idカラムにbookローカル変数で取得したデータを代入できる箱を用意し、ローカル変数commentに格納する
     # 以下分解
     # １．favorite = Favorite.new(book_id: book.id)
@@ -10,14 +10,16 @@ class FavoritesController < ApplicationController
     # ２．favorite.user_id = current_user.id
     # 　　１で作成された変数favoriteに格納されたFavoriteモデルのuseridカラムにcurrent_userのidを代入
     favorite.save
-    redirect_to request.referer
+    
+    # ↓非同期通信なので削除
+    # redirect_to request.referer
     # ユーザーがアクセスした前のページのURLを取得する = books/:id/favoriteの後にまたbooks/:idを取得する
   end
   
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 end
